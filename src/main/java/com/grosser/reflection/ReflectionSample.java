d38d61f448ae67ad3cdd115b6d9bd1d4c08e7cf5
Joshua Grosser <joshua.grosser@gmail.com>
2013-03-03 12:21:58 -0500

    Added an advanced reflection sample.

--- src/main/java/com/grosser/reflection/ReflectionSample.java
+++ src/main/java/com/grosser/reflection/ReflectionSample.java
@@ -1,9 +1,10 @@
 package com.grosser.reflection;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.*;
+import java.util.*;
+
+import static java.lang.System.out;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,10 +13,9 @@ import java.util.Set;
  * Time: 11:09 AM
  * To change this template use File | Settings | File Templates.
  */
-public class ReflectionSample {
+public class ReflectionSample implements Runnable {
 
     private static ReflectionSample instance;
-
     private enum SomeEnum { ITEM_A, ITEM_B }
 
     /**
@@ -24,10 +24,16 @@ public class ReflectionSample {
      */
     public static void main(String[] args){
         instance = new ReflectionSample();
-        instance.doSomething();
+        instance.run();
     }
 
-    public void doSomething(){
+    @Override
+    public void run() {
+        runBasicSamples();
+        runAdvancedSamples();
+    }
+
+    private void runBasicSamples(){
 
         // Returns "class java.lang.String"
         Class clazz = String.class;
@@ -60,6 +66,71 @@ public class ReflectionSample {
             System.out.println("Class for HashMap using forName(): " + hashMapClazz.toString());
         } catch (ClassNotFoundException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    private void runAdvancedSamples(){
+
+        try {
+            Class<?> c = Class.forName("java.util.HashMap");
+            out.println("\n\n\n********************** Advanced Sample **********************");
+            out.format("Class:%n  %s%n%n", c.getCanonicalName());
+            out.format("Modifiers:%n  %s%n%n",
+                    Modifier.toString(c.getModifiers()));
+
+            out.format("Type Parameters:%n");
+            TypeVariable[] tv = c.getTypeParameters();
+            if (tv.length != 0) {
+                out.format("  ");
+                for (TypeVariable t : tv)
+                    out.format("%s ", t.getName());
+                out.format("%n%n");
+            } else {
+                out.format("  -- No Type Parameters --%n%n");
+            }
+
+            out.format("Implemented Interfaces:%n");
+            Type[] intfs = c.getGenericInterfaces();
+            if (intfs.length != 0) {
+                for (Type intf : intfs)
+                    out.format("  %s%n", intf.toString());
+                out.format("%n");
+            } else {
+                out.format("  -- No Implemented Interfaces --%n%n");
+            }
+
+            out.format("Inheritance Path:%n");
+            List<Class> l = new ArrayList<Class>();
+            printAncestor(c, l);
+            if (l.size() != 0) {
+                for (Class<?> cl : l)
+                    out.format("  %s%n", cl.getCanonicalName());
+                out.format("%n");
+            } else {
+                out.format("  -- No Super Classes --%n%n");
+            }
+
+            out.format("Annotations:%n");
+            Annotation[] ann = c.getAnnotations();
+            if (ann.length != 0) {
+                for (Annotation a : ann)
+                    out.format("  %s%n", a.toString());
+                out.format("%n");
+            } else {
+                out.format("  -- No Annotations --%n%n");
+            }
+
+            // production code should handle this exception more gracefully
+        } catch (ClassNotFoundException x) {
+            x.printStackTrace();
+        }
+    }
+
+    private static void printAncestor(Class<?> c, List<Class> l) {
+        Class<?> ancestor = c.getSuperclass();
+        if (ancestor != null) {
+            l.add(ancestor);
+            printAncestor(ancestor, l);
         }
     }
 
