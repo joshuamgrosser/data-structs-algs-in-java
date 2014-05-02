efa3aceeb46fcd896d6d169a327bd7bd56e43841
Joshua Grosser <joshua.grosser@gmail.com>
2014-05-02 15:13:43 -0400

    Adding JUnit 4

---
+++ src/main/test/com/grosser/ClientTest.java
@@ -0,0 +1,30 @@
+package com.grosser;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Package: com.grosser
+ * Project: data-structs-algs-in-java
+ * User: @author joshuagrosser
+ * Date: 5/2/14
+ * Time: 2:59 PM
+ */
+public class ClientTest {
+
+    @Before
+    public void before(){
+
+    }
+
+    @After
+    public void after() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
